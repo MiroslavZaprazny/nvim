f81f37a7a6c5fb7484d179a39fab7fd2d2dc0d56
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-05-16 13:22:52 +0200

    Add git blame

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -75,6 +75,7 @@ return packer.startup(function(use)
 	-- use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
 	use("lewis6991/gitsigns.nvim")
+	use("f-person/git-blame.nvim")
 
 	--colorschemes
 	-- use("morhetz/gruvbox")
